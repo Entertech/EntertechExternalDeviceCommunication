63dc9eb9b87fa4b291e53b684703b5beae22b8c4
wangkang <1226426603@qq.com>
2023-11-08 11:54:59 +0800

    定义接口

--- entertech_communication_api/src/main/java/cn/entertech/communication/api/IExternalDevice.kt
+++ entertech_communication_api/src/main/java/cn/entertech/communication/api/IExternalDevice.kt
@@ -1,6 +1,6 @@
 package cn.entertech.communication.api
 
-import android.content.Context
+import cn.entertech.communication.bean.ExternalDeviceType
 
 /**
  * 外部设备
@@ -10,26 +10,28 @@ interface IExternalDevice {
     fun setExternalDeviceListener(listener: IExternalDeviceListener)
 
     fun addConnectListener(listener: (String) -> Unit)
-
     fun removeConnectListener(listener: (String) -> Unit)
 
     fun addRawDataListener(rawListener: ((ByteArray) -> Unit))
+    fun removeRawDataListener(listener: (ByteArray) -> Unit)
 
     fun addHeartRateListener(heartRateListener: ((Int) -> Unit))
+    fun removeHeartRateListener(heartRateListener: ((Int) -> Unit))
 
     fun addContactListener(contactListener: ((Int) -> Unit))
+    fun removeContactListener(contactListener: ((Int) -> Unit))
 
-    fun connect(context: Context)
-
+    fun connect()
     fun disConnect()
 
     fun startHeartAndBrainCollection()
-
     fun stopHeartAndBrainCollection()
 
     fun write(byteArray: ByteArray)
 
     fun read(byteArray: ByteArray): Int
+
+    fun getExternalDeviceType():ExternalDeviceType
 }
 
 interface IExternalDeviceListener {
