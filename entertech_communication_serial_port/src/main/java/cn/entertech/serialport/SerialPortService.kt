9a37ec39f50a7ced56676c3e5db88e6241d85b50
wangkang <1226426603@qq.com>
2023-11-21 11:26:39 +0800

    子线程，避免anr

--- entertech_communication_serial_port/src/main/java/cn/entertech/serialport/SerialPortService.kt
+++ entertech_communication_serial_port/src/main/java/cn/entertech/serialport/SerialPortService.kt
@@ -1,21 +1,15 @@
 package cn.entertech.serialport
 
-import android.app.Service
+import android.app.IntentService
 import android.content.Intent
-import android.os.IBinder
 import android.serialport.SerialPort
 import cn.entertech.serialport.SerialPortCommunicationManage.Companion.SERIAL_PORT_HANDSHAKE_END
 
-class SerialPortService : Service() {
+class SerialPortService : IntentService("SerialPortService") {
 
-    override fun onBind(intent: Intent): IBinder? {
-        return null
-    }
-
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+    @Deprecated("Deprecated in Java")
+    override fun onHandleIntent(intent: Intent?) {
         SerialPort.init()
         sendBroadcast(Intent(SERIAL_PORT_HANDSHAKE_END))
-        return super.onStartCommand(intent, flags, startId)
     }
 }
