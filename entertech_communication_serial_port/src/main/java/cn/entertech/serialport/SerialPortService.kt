d986174b584db03230ebb36109d33c8ca7fdae66
wangkang <1226426603@qq.com>
2023-11-22 09:30:47 +0800

    日志

--- entertech_communication_serial_port/src/main/java/cn/entertech/serialport/SerialPortService.kt
+++ entertech_communication_serial_port/src/main/java/cn/entertech/serialport/SerialPortService.kt
@@ -3,12 +3,18 @@ package cn.entertech.serialport
 import android.app.IntentService
 import android.content.Intent
 import android.serialport.SerialPort
+import cn.entertech.communication.log.ExternalDeviceCommunicateLog
 import cn.entertech.serialport.SerialPortCommunicationManage.Companion.SERIAL_PORT_HANDSHAKE_END
 
 class SerialPortService : IntentService("SerialPortService") {
 
+    companion object{
+        private const val TAG="SerialPortService"
+    }
+
     @Deprecated("Deprecated in Java")
     override fun onHandleIntent(intent: Intent?) {
+        ExternalDeviceCommunicateLog.d(TAG,"SerialPortService init")
         SerialPort.init()
         sendBroadcast(Intent(SERIAL_PORT_HANDSHAKE_END))
     }
