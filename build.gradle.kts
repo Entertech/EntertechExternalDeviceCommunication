641bea1ada0abf20b0d706b6142460903159362a
wangkang <1226426603@qq.com>
2023-11-22 10:28:14 +0800

    移除发布插件

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
-    dependencies {
-        classpath("custom.android.plugin:publish:0.0.2")
-    }
-}
 
 plugins {
     id("com.android.application") version "8.1.0" apply false
