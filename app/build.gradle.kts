0411c41087c755be2386b3ba454ebaeef7d6a070
wangkang <1226426603@qq.com>
2023-12-28 15:42:27 +0800

    使用内部组件库

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+//        sourceCompatibility = JavaVersion.VERSION_1_8
+//        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
@@ -37,7 +37,7 @@ android {
 
 dependencies {
 //    implementation fileTree(dir:"libs',include: ['*.jar','*.aar'])
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar"))))
+//    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar"))))
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
@@ -45,5 +45,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(project(mapOf("path" to ":entertech_communication_serial_port")))
+    implementation(project(mapOf("path" to ":entertech_communication_usb")))
+    implementation(project(mapOf("path" to ":entertech_communication_api")))
 //    implementation ("cn.entertech.android:device_communicate_serialport:0.0.1")
 }
