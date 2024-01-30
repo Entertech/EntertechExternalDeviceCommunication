cb9e07458525aa7b879af9b3beab18c6618d6fbd
wangkang <1226426603@qq.com>
2024-01-30 16:47:39 +0800

    添加注释

--- entertech_communication_api/src/main/java/cn/entertech/communication/api/IProcessDataHelper.kt
+++ entertech_communication_api/src/main/java/cn/entertech/communication/api/IProcessDataHelper.kt
@@ -2,6 +2,13 @@ package cn.entertech.communication.api
 
 interface IProcessDataHelper {
 
+    /**
+     * @param byteInt 读取出来的字节
+     * @param contactListeners 佩戴监听 is [BaseExternalDeviceCommunicationManage.contactListeners]
+     * @param bioAndAffectDataListeners 生物基础数据&情感数据监听 is [BaseExternalDeviceCommunicationManage.bioAndAffectDataListeners]
+     * @param heartRateListeners 心率监听 is [BaseExternalDeviceCommunicationManage.heartRateListeners]
+     * @param finish 拿到完整的数据包结构时候的回调
+     * */
     fun process(
         byteInt: Byte,
         contactListeners: List<((Int) -> Unit)?>,
