6261e949c8ea9dcc8068be534383bf10e8baa961
wangkang <1226426603@qq.com>
2024-02-22 10:18:05 +0800

    使用本地依赖

--- entertech_communication_serial_port/build.gradle.kts
+++ entertech_communication_serial_port/build.gradle.kts
@@ -51,9 +51,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-//    implementation(project(mapOf("path" to ":entertech_communication_api")))
+    implementation(project(mapOf("path" to ":entertech_communication_api")))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    api ("cn.entertech.android:device_communicate_api:0.0.5")
+//    api ("cn.entertech.android:device_communicate_api:0.0.5")
 }
